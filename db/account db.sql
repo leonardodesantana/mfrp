60d5b7a74defe2cac62c3e6187dc096f2c250d4a
leonardodesantana <leonardochk@gmail.com>
2024-06-25 22:39:47 -0300

    User Account SQL db created

---
+++ db/account db.sql
@@ -0,0 +1,13 @@
+create database if not exists mfrp;
+use mfrp;
+create table if not exists users(
+	accountId INT auto_increment primary key,
+	userName VARCHAR(50) not null unique,
+	userEmail VARCHAR(100) not null unique,
+	hashedPassword VARCHAR(255) not null,
+	salt VARCHAR(255) not null,
+	hwSerial VARCHAR(100) not null unique,
+	creationDate TIMESTAMP not null,
+	lastLogin TIMESTAMP not null
+);
+
