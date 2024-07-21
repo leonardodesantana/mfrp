e1bf5a373dac3c92f292c5f27505cac25c4ba2f1
leonardodesantana <leonardochk@gmail.com>
2024-07-21 11:16:58 -0300

    Debug output created to check if DB connection is working as intended.

--- mods/deathmatch/resources/account-system/s_account.lua
+++ mods/deathmatch/resources/account-system/s_account.lua
@@ -1,4 +1,10 @@
 -- Database connection established.
 addEventHandler("onResourceStart", root, function()
     dbConnection = dbConnect("sqlite",":/db/mfrp.db")
+    if dbConnection then
+        outputDebugString("DB connection was successful.")
+    else
+        outputDebugString("DB connection failed.")
+    end
 end)
+
